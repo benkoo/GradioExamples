0496835fe2e0be436e3e1e584300ea36d1076d5a
Ben Koo <koo0905@gmail.com>
2024-03-05 12:13:11 +0800

    Added app.py's partial implementation

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ import numpy as np
 model = tf.keras.models.load_model('model.h5')
 
 
-def resize_image(image, new_width, new_height):
-    resized_image = image.resize((new_width, new_height))
-    return resized_image
+def resize_to_pixel_count(image, pixel_count):
+    width, height = image.size
+    new_size = (pixel_count, pixel_count) 
+    image = image.resize(new_size, Image.NEAREST)
+    return image
+
 
 def recognize_digit(image):
     convertedFromSketchPad = None
@@ -20,17 +23,15 @@ def recognize_digit(image):
             layer_image = convert_to_pil_image(layer)
             convertedFromSketchPad.paste(layer_image, (0, 0), layer_image)  
 
-    low_res = resize_image(convertedFromSketchPad, 28, 28)
-    print("The low_res image size is: " + str(low_res.size))
-    low_res = low_res.convert('L') 
-    img_array = np.array(low_res)
+    low_res = resize_to_pixel_count(convertedFromSketchPad, 28)
+    image = np.array(low_res, dtype='float32')
     
-    if img_array is not None:  # flatten to 1D
-        image = img_array.reshape((1, 28, 28, 1)).astype('float32') / 255
+    if image is not None:  # flatten to 1D
+        image = image.reshape((1, 28, 28, 1)).astype('float32') / 255.0
         prediction = model.predict(image)
-        return {str(i): float(prediction[0][i]) for i in range(10)}
+        return [{str(i): float(prediction[0][i]) for i in range(10)}, low_res]
     else:
-        return ''
+        return ['', low_res]
     
 
 def convert_to_pil_image(image_data):
@@ -38,10 +39,11 @@ def convert_to_pil_image(image_data):
         return image_data
     elif isinstance(image_data, str):  # Assuming filepath
         return Image.open(image_data)
-    elif isinstance(image_data, np.ndarray):  
+    elif isinstance(image_data, np.ndarray): 
         return Image.fromarray(image_data)
     else:
         raise ValueError("Unsupported image representation")
+
     
 def process_sketch(sketch_dict):
 
@@ -56,11 +58,12 @@ def process_sketch(sketch_dict):
         return background 
     else:
         raise ValueError("This is not sketchpad data.")
+
         
 iface = gr.Interface(
-    fn = recognize_digit,
-    inputs = gr.Sketchpad(),
-    outputs = gr.Label(num_top_classes=3),
+    fn=recognize_digit,
+    inputs=gr.Sketchpad(),
+    outputs=[gr.Label(num_top_classes=10), gr.Image()],
 )
 
 iface.launch()
